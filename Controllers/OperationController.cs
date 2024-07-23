dee1a5124d0eb6e4e83710174b0db94798bbd011
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2024-07-23 10:59:48 +0530

    Add files via upload

---
+++ Controllers/OperationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WelcomeProject.Models;
+
+namespace WelcomeProject.Controllers
+{
+    public class OperationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AddModel obj,IFormCollection obj1)
+        {
+            int a = obj.Num1;
+            int b = obj.Num2;
+            int c=0;
+            String ope = obj1["btnsubmit"].ToString();
+            if(ope=="+")
+            {
+                c = a + b;
+            }
+            else if(ope=="-")
+            {
+                c=a- b;
+            }
+            else if(ope== "*")
+            {
+                c = a * b;
+            }
+            else if(ope== "/")
+            {
+                c = a / b;
+            }
+            ViewBag.result = "Result is "+c;
+            return View();
+        }
+    }
+}
---
+++ Models/AddModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WelcomeProject.Models
+{
+    public class AddModel
+    {
+        private int num1;
+        private int num2;
+
+        public int Num1 { get => num1; set => num1 = value; }
+        public int Num2 { get => num2; set => num2 = value; }
+    }
+}
